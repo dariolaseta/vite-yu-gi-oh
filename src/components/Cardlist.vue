7515552246ff4877b0aad7c511d298d750d4de9b
Oxidra <dantelezzo@gmail.com>
2023-06-19 13:40:52 +0200

    init

---
+++ src/components/Cardlist.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Cardlist",
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Cards",
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+    
+</style>
