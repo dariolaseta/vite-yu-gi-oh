a84fc55d37ec6a5db14d8c4b43e2f1c9e40f37bc
Oxidra <dantelezzo@gmail.com>
2023-06-19 13:51:20 +0200

    added api

--- src/components/Cardlist.vue
+++ src/components/Cardlist.vue
@@ -3,12 +3,21 @@
 </template>
 
 <script>
+import axios from 'axios';
+import Cards from "./Cards.vue"
+import {store} from "../store.js"
+
 export default {
     name: "Cardlist",
     data(){
         return {
-
+            api: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+            cardList: [],
+            store
         }
+    },
+    components:{
+        Cards
     }
 }
 </script>
