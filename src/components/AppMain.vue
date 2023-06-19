27288b5327d9675641d24df449da2ddcbb2ff929
Oxidra <dantelezzo@gmail.com>
2023-06-19 14:27:03 +0200

    created test card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,6 +6,18 @@
                     <p>Found n cards</p>
                 </div>
             </div>
+
+            <div class="cards-container">
+                <div class="card">
+                    <div class="img">
+
+                    </div>
+                    <div class="test-p">
+                        <p>"A" cell breeding device</p>
+                        <p>Alien</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 </template>
@@ -19,23 +31,52 @@ export default {
 
         div.container{
             background-color: white;
-            display: flex;
-            justify-content: center;
-            height: 900px;
+
+            height: 100%;
             width: 1200px;
             margin: 3rem auto 0;
+            padding-top: 3rem;
 
             div.cards-header{
                 display: flex;
                 align-items: center;
                 width: 80%;
                 height: 50px;
-                margin-top: 3rem;
+                margin: 0 auto;
                 background-color: #212529;
                 
                 div.text{
                     p{
                         margin-left: 1rem;
+                    }
+                }
+            }
+
+            div.cards-container{
+                margin: 0 auto;
+                width: 80%;
+                height: 900px; //rimuovere
+                background-color: red;
+                display: flex;
+                flex-wrap: wrap;
+
+                div.card{
+                    height: 300px;
+                    width: 200px;
+                    background-color: blue;
+
+                    div.img{
+                        height: 70%;
+                        width: 100%;
+                        background-color: black;
+                    }
+
+                    div.test-p{
+
+                        p{
+                            margin: 1rem;
+                            text-align: center;
+                        }
                     }
                 }
             }
