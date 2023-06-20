3934f02ff7a2eea8b274cb9a9315a85eb1660a70
Oxidra <dantelezzo@gmail.com>
2023-06-20 12:04:57 +0200

    created appsearch

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
   import AppMain from "./components/AppMain.vue"
   import AppHeader from "./components/AppHeader.vue"
+  import AppSearch from "./components/AppSearch.vue"
 
   export default{
     name: "App",
     components: {
       AppHeader,
       AppMain,
+      AppSearch
     }
   }
 </script>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "AppSearch",
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
